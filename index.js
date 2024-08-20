eee82bc79c2a58aea3c17b2714e8c59695271e78
ishanrana363 <ishanrana363@gmail.com>
2024-08-20 13:56:35 +0600

    vercel update

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 
 const port = process.env.PORT||8000
 
-app.listen(port,async ()=>{
+app.listen(port, ()=>{
     console.log(`Server run successfully at http://localhost:${port}`);
-    await connectDb();
+     connectDb();
 });
